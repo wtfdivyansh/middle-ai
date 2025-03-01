b449c002fcfafef129643dec2a7fcb623418a403
rxhul18 <rahulshah0728@gmail.com>
2025-03-02 00:58:08 +0530

    Added IntroVideo component with YouTube video dialog

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,6 +2,7 @@
 import AutomateReceipt from "@/components/custom/site/automate-receipt";
 import { Footer } from "@/components/custom/site/footer/footer";
 import Hero from "@/components/custom/site/hero";
+import IntroVideo from "@/components/custom/site/introvideo";
 import StressFree from "@/components/custom/site/stressfree";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <div className="mt-20">
       <Hero />
       <AutomateReceipt />
+      <IntroVideo />
       <StressFree />
       <Footer />
     </div>
